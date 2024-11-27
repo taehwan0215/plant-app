ab9bed9e085134f6e496fec85492925dd3c566d5
taehwan0215 <taehwan0215@naver.com>
2024-11-28 00:35:10 +0900

    #frontend

--- app/src/main/java/com/example/plant2/models/Comment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.plant2.models
-
-data class Comment(
-    val commentId: Int,
-    val content: String,
-    val authorId: Int,
-    val parentCommentId: Int?,
-    val createdAt: String
-)
